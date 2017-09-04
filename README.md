7476c9a2ed60dc1c07ca2ce392f08c2845a19041
Stephan H. Wissel <stw@linux.com>
2017-09-04 22:56:53 +1000

    Readme and license update

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Excel2XML
-Small utility that exports Excel lists to XML for easy processing with XSLT transformations
+Small utility that exports Excel lists to XML for easy processing with XSLT transformations.
+
+Business loves to maintain all sorts of Excel lists. Excel sort and filter capabilities make this a conventient list store.
+
+However when some more elaborate sorting and grouping is required and formatted reports are needed, Excel falls flat.
+
+This utility, uses Apache POI to export an Excel file into a simplified XML format that:
+
+ - treats the first row as column labels
+ - carries row and column numbers as cell properties
+ - carries the column label (if there was one) as title attribute
+ 
+ This enables a simplified creation of XML stylesheets with XPath syntax.
+ 
+ This isn't a sophisticated tool, but an itch I had to scratch.
+ 
+ (C) 2017 St. Wissel, for license see LICENSE file
--- src/main/java/net/wissel/tools/excel/E2xCmdline.java
+++ src/main/java/net/wissel/tools/excel/E2xCmdline.java
@@ -1,3 +1,19 @@
+/**
+ * Command Line Tool to extract Excel Lists to XML
+ * 
+ * Copyright 2017 St. Wissel
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
 package net.wissel.tools.excel;
 
 import java.io.File;
@@ -18,7 +34,6 @@ import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
-import javax.xml.transform.OutputKeys;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
